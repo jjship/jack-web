72371f0f7d084e0800f5f9e71a1469bddb629c06
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-01-26 18:57:51 +0100

    changed / to hero.hbs

--- src/routes/root.js
+++ src/routes/root.js
@@ -2,6 +2,6 @@ module.exports = async (fastify, opts) => {
   // sprawdza czy w ciastkach jest flaga wieku, jeśli nie to /age
   fastify.get('/', (request, reply) => {
     // return await selectAll();
-    reply.view('age.hbs', {});
+    reply.view('hero.hbs', {});
   });
 };
