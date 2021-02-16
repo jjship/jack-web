5a08a85d519b578f5b769790337ffa4825c9e664
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-02-16 12:51:52 +0100

    del clg

--- apka/src/public/controllers/collapsible.js
+++ apka/src/public/controllers/collapsible.js
@@ -5,7 +5,6 @@ const arr = document.getElementById('arrow');
 togg.addEventListener('click', () => {
   if ('mniej' == togg.innerHTML) {
     togg.innerHTML = togg.getAttribute('data-text');
-    console.log(togg.innerHTML);
     arr.classList.toggle('down');
   } else {
     togg.setAttribute('data-text', togg.innerHTML);
