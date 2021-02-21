38389b7f4ba0e2c1e7ad2773b632d2940a45707c
ActivistGames <74770374+ActivistGames@users.noreply.github.com>
2021-02-21 21:07:05 +0100

    różne fonty, różne wielkości tekstu

--- apka/jackDaniels_interakcja_cien/jackDaniels_interakcja_cien.js
+++ apka/jackDaniels_interakcja_cien/jackDaniels_interakcja_cien.js
@@ -1,11 +1,12 @@
-let zdania = ["mając 40 lat pierwszy raz poszedłem na balet", "tańczyć", "pójść na balet", "sanki", "wagary", "uprawiałem seks w miejscu publicznym", "nauczyłam się boksować", "pierwszy raz pocałowałem dziewczynę w saunie", "pierwszy raz paliłem trawę ze znajomymi na wakacjach", "odważyłam się zaśpiewać pierwszy raz na moich urodzinach", "pierwszy raz ostatnio wziąłem udział w zawodach", "wziąłem kredyt", "odnalazłem moją prawdziwą mamę"];
+let zdania = ["zostanę bohaterem", "nauczę się tańczyć", "pójdę na balet", "wakacje", "wagary", "seks w miejscu publicznym", "nauczę się boksować", "pocałuję dziewczynę w saunie", "zapalę trawę ze znajomymi na wakacjach", "zaśpiewam na moich urodzinach", "wezmę udział w zawodach", "wezmę kredyt", "odnajdę moją prawdziwą mamę"];
 let zdaniaWyswietlane = [];
 let fontJasper;
 let fontMotlow;
 let fontFranklin;
-let szerokoscButelki = 250; 
+
+let szerokoscButelki = 220; 
 let wysokoscButelki = 774;
-let startTekst = 0;
+let startTekstX = 20;
 let maska;
 let t=0;
 let zi=0;
@@ -13,7 +14,8 @@ let zi=0;
 function preload() {
   fontJasper = loadFont("font/JasperPl-Caps.otf");  
   fontMotlow = loadFont("font/Motlow.otf");  
-  fontFranklin = loadFont("font/FranklinGothic.ttf");  
+  fontFranklin = loadFont("font/Franklin.ttf");  
+
   maska = loadImage('cien_maska.png');
 }
 
@@ -26,16 +28,8 @@ function setup() {
 function draw() {
   t++;
   background(255);
-  /*
-  text(zdania[0], width/2, 100);//jakaś część tekstów
-   filter(BLUR, 1);
-   text(zdania[1], width/2, 200); //jakasś inna część
-   filter(BLUR, 1);
-   text(zdania[2], width/2, 300); //ostatnia część
-   */
 
-
-  if (t%90==0) {
+  if (t%220==0) {
     zdaniaWyswietlane.push(new Zdanie(zdania[zi]));
     zi++;
     if (zi>=zdania.length) {
@@ -49,89 +43,133 @@ function draw() {
     zdaniaWyswietlane[i].show();
 
     if (zdaniaWyswietlane[i].martwy) {
-      print(i);
       zdaniaWyswietlane.splice(i, 1);
     }
   }
+  
+  //dodać zdania nadlatujące z dwóch boków
+  
+  //dodać blur
+  
+  //dodać interakcję z kursorem
+  
   image(maska, 0, 0);
 }
 
 
 class Zdanie {
   constructor(txt) {
-    this.x = 0;
-    this.y = wysokoscButelki;
-    this.s = 22;//to powinno być zmienne
-    this.txt = this.podzialNaLinie(txt, this.s);
-    this.sx = random(-0.2, 0.2);
-    this.sy = random(0.5, 0.9);
-    this.f = random(50, 120);
+    this.x = startTekstX;
+    this.y = wysokoscButelki+40;
+    this.font = this.wybierzFont(txt);
+    this.txt = this.podzialNaLinie(txt, this.font);    
+
+    if (this.txt.length > 2) {
+      let granice1 = this.font.textBounds(this.txt[0], startTekstX, 0, int(this.txt[2]));
+      let granice2 = this.font.textBounds(this.txt[1], startTekstX, 0, int(this.txt[3]));
+      this.y2 = granice2.h - (granice2.h + granice2.y) + granice1.h + granice1.y; //tyle trzeba dodać do y pierwszej linijki, żeby znaleźć y drugiej linijki
+    }
+
+    this.sx = random(-0.1, 0.1);
+    this.sy = random(0.4, 0.6);
+    this.f = random(50, 120);//przeźroczystość
     this.martwy = false;
   }
 
   show() {
-    textSize(this.s);
-    textFont(fontJasper);//to powinno być zmienne
-    fill(this.f);
-    text(this.txt[0], this.x, this.y);//początek na dole, a póżniej leci w góre
-    if (this.txt.length > 0) {//jeśli jest druga linijka
-      text(this.txt[1], this.x, this.y+this.s);
+    textFont(this.font);//to powinno być zmienne
+    noStroke();
+    fill(0, this.f);
+
+    if (this.txt.length == 2) {
+      textSize(int(this.txt[1]));
+      text(this.txt[0], this.x, this.y+50);//początek na dole, a póżniej leci w góre
+    } else {//jeśli jest druga linijka
+      textSize(int(this.txt[2]));
+      text(this.txt[0], this.x, this.y);
+      textSize(int(this.txt[3]));
+      text(this.txt[1], this.x, this.y+this.y2);
     }
-   
+
     this.x += this.sx;
     this.y -= this.sy;
 
-    if (this.y<0) {
+    if (this.y<-200) {
       this.martwy = true;
     }
   }
 
-  podzialNaLinie(txt, s) {
-    let pierwszaLinijka = txt;
-    let drugaLinijkaPodzielona = [];
-    let drugaLinijka;
-
-    let fj = fontJasper;
-
+  podzialNaLinie(txt, font) {
+    let pierwszaLinijka = txt;    
     let podzieloneZdanie = split(txt, ' ');  
-    //let pierwszeSlowo = podzieloneZdanie.shift();
+    let ostatnieSlowo = podzieloneZdanie.pop();//usuwam ostatnie słowo z podzieloneZdanie
+    let wielkoscOstatnia = 200;
+    let wielkoscPierwsza = 200;
 
-    //let czySieMiesci = sprawdzGranice(pierwszeSlowo, s);
-    //if (!czySieMiesci) {    
-    // yy += s;
-    // }
+    let czySieMiesci = this.sprawdzGranice(ostatnieSlowo, wielkoscOstatnia, font);
 
-    let czySieMiesci = this.sprawdzGranice(pierwszaLinijka, s);
+    while (!czySieMiesci) {
+      wielkoscOstatnia--;
+      czySieMiesci = this.sprawdzGranice(ostatnieSlowo, wielkoscOstatnia, font);
+    }     
 
     let linijki = [];
 
-    if (czySieMiesci) {   
+    if (podzieloneZdanie.length == 0) {   
       linijki[0] = txt;
+      linijki[1] = str(wielkoscOstatnia);
       return linijki;
     } else {
+      pierwszaLinijka = join(podzieloneZdanie, ' '); //cały tekst bez ostatniego słowa
+      czySieMiesci = this.sprawdzGranice(pierwszaLinijka, wielkoscPierwsza, font);
       while (!czySieMiesci) {
-        podzieloneZdanie = split(pierwszaLinijka, ' ');       
-        drugaLinijkaPodzielona.unshift(podzieloneZdanie.pop());//dodaje na początek tablicy ostatnie słowo i usuwa go z podzieloneZdanie
-        pierwszaLinijka = join(podzieloneZdanie, ' ');
-        czySieMiesci = this.sprawdzGranice(pierwszaLinijka, s);
+        wielkoscPierwsza--;
+        czySieMiesci = this.sprawdzGranice(pierwszaLinijka, wielkoscPierwsza, font);
       }
-      drugaLinijka = join(drugaLinijkaPodzielona, ' '); 
-      //sprawdzić czy mieści się druga linijka, jeśli nie, to zmniejszyć font i powtórzyć proces
 
       linijki[0] = pierwszaLinijka;
-      linijki[1] = drugaLinijka;
+      linijki[1] = ostatnieSlowo;
+      linijki[2] = str(wielkoscPierwsza);
+      linijki[3] = str(wielkoscOstatnia);
       return linijki;
     }
   }
 
-  sprawdzGranice(txt, s) {
-    let fj = fontJasper;
-
-    let granice = fj.textBounds(txt, startTekst, 0, s);
+  sprawdzGranice(txt, s, font) {
+    let granice = font.textBounds(txt, startTekstX, 0, s);
     if (granice.x + granice.w > szerokoscButelki) {
       return false;
     } else {
       return true;
     }
   }
+
+  wybierzFont(txt) {
+    let litery = split(txt, ''); 
+    let ileLiter = litery.length;
+    let fonty = [];
+    let ł = false;
+
+    for (let i = 0; i<ileLiter; i++) {
+      if (litery[i] == "ł" || litery[i] == "Ł") {
+        ł = true;
+      }
+    }
+    
+    if (ł) {
+        fonty = [fontJasper, fontFranklin];
+      } else {
+        fonty = [fontJasper, fontFranklin, fontMotlow];
+      }
+ 
+    return fonty[floor(random(fonty.length))];
+  }
 }
+
+/*
+  text(zdania[0], width/2, 100);//jakaś część tekstów
+ filter(BLUR, 1);
+ text(zdania[1], width/2, 200); //jakasś inna część
+ filter(BLUR, 1);
+ text(zdania[2], width/2, 300); //ostatnia część
+ */
