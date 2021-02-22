91c2e35dd76f1e9cacd30c0a1a96c2b59c7e289a
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-02-22 15:12:41 +0100

    set markup, classes, styles and js for mobile nav

--- apka/src/public/controllers/collapsible.js
+++ apka/src/public/controllers/collapsible.js
@@ -1,16 +1,19 @@
-const togg = document.getElementById('toggle');
-const nav = document.getElementById('navi');
-const arr = document.getElementById('arrow');
+const togg = document.querySelector('.js-toggle');
+const nav = document.querySelector('.js-nav');
+const arr = document.querySelector('.js-arrow');
+console.log(togg);
+console.log(nav);
+console.log(arr);
 
 togg.addEventListener('click', () => {
   if ('mniej' == togg.innerHTML) {
     togg.innerHTML = togg.getAttribute('data-text');
-    arr.classList.toggle('down');
+    arr.classList.toggle('h-is__down');
   } else {
     togg.setAttribute('data-text', togg.innerHTML);
     togg.innerHTML = 'mniej';
-    arr.classList.toggle('down');
+    arr.classList.toggle('h-is__down');
   }
 
-  nav.classList.toggle('closed');
+  nav.classList.toggle('h-is__closed');
 });
