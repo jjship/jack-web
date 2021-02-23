91c3b1679d30a863e785c50da4fc7213f474f8ce
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-02-23 12:55:10 +0100

    correct nav display on 1450px width

--- apka/src/public/controllers/collapsible.js
+++ apka/src/public/controllers/collapsible.js
@@ -1,19 +1,19 @@
 const togg = document.querySelector('.js-toggle');
 const nav = document.querySelector('.js-nav');
 const arr = document.querySelector('.js-arrow');
-console.log(togg);
-console.log(nav);
-console.log(arr);
+
+const setArrow = () => {
+  if (nav.classList.contains('h-is__closed')) {
+    togg.innerHTML - 'mniej';
+    arr.classList.remove('h-is__down');
+    nav.classList.toggle('h-is__closed');
+  } else {
+    togg.innerHTML - 'menu';
+    arr.classList.add('h-is__down');
+    nav.classList.toggle('h-is__closed');
+  }
+};
 
 togg.addEventListener('click', () => {
-  if ('mniej' == togg.innerHTML) {
-    togg.innerHTML = togg.getAttribute('data-text');
-    arr.classList.toggle('h-is__down');
-  } else {
-    togg.setAttribute('data-text', togg.innerHTML);
-    togg.innerHTML = 'mniej';
-    arr.classList.toggle('h-is__down');
-  }
-
-  nav.classList.toggle('h-is__closed');
+  setArrow();
 });
--- apka/src/public/controllers/desktopClasses.js
+++ apka/src/public/controllers/desktopClasses.js
@@ -1,25 +1,29 @@
+const dsk = '(min-width: 1450px)';
+const mob = '(max-width: 1449px)';
+
 const changeClass = (mediaQuery) => {
-  if (mediaQuery.matches) {
-    // If media query matches
-    nav.classList.remove('h-is__closed');
-  } else {
-    nav.classList.add('h-is__closed');
-  }
+  mediaQuery.matches
+    ? nav.classList.add('h-is__closed')
+    : nav.classList.remove('h-is__closed');
 };
 
 const lights = document.querySelector('.bg_lights');
 
-const changePicture = (mediaQuery) => {
-  mediaQuery.matches
-    ? (lights.src = '/images/BG_lights_desktop_main.png')
-    : (lights.src = '/images/BG_lights.png');
-};
+// const changePicture = (mediaQuery) => {
+//   mediaQuery.matches
+//     ? (lights.src = '/images/BG_lights_desktop_main.png')
+//     : (lights.src = '/images/BG_lights.png');
+// };
 
-var mediaQuery = window.matchMedia('(min-width: 1450px)');
+const mq_dsk = window.matchMedia(dsk);
+const mq_mob = window.matchMedia(mob);
 
 // Call listener function at run time
-changeClass(mediaQuery);
-changePicture(mediaQuery);
+changeClass(mq_dsk);
+setArrow(mq_mob);
+// changePicture(mediaQuery);
 
 // Attach listener function on state changes
-mediaQuery.addListener(changePicture);
+// mediaQuery.addListener(changePicture);
+mq_dsk.addListener(changeClass);
+mq_mob.addListener(setArrow);
