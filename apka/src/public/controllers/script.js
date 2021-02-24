c9c8ce0650474adbd088e459374403879362a141
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-02-24 20:55:28 +0100

    order

--- apka/src/public/controllers/script.js
+++ apka/src/public/controllers/script.js
@@ -1,6 +1,11 @@
 const togg = document.querySelector('.js-toggle');
 const nav = document.querySelector('.js-nav');
 const arr = document.querySelector('.js-arrow');
+const lights = document.querySelector('.bg_lights');
+const dsk = '(min-width: 1450px)';
+const mob = '(max-width: 1449px)';
+const mq_dsk = window.matchMedia(dsk);
+const mq_mob = window.matchMedia(mob);
 
 const setArrow = () => {
   if (nav.classList.contains('h-is__closed')) {
@@ -14,29 +19,17 @@ const setArrow = () => {
   }
 };
 
-togg.addEventListener('click', () => {
-  setArrow();
-});
-
-const dsk = '(min-width: 1450px)';
-const mob = '(max-width: 1449px)';
-
 const changeClass = (mediaQuery) => {
   mediaQuery.matches
     ? nav.classList.add('h-is__closed')
     : nav.classList.remove('h-is__closed');
 };
 
-const lights = document.querySelector('.bg_lights');
-
 // const changePicture = (mediaQuery) => {
 //   mediaQuery.matches
 //     ? (lights.src = '/images/BG_lights_desktop_main.png')
 //     : (lights.src = '/images/BG_lights.png');
 // };
-
-const mq_dsk = window.matchMedia(dsk);
-const mq_mob = window.matchMedia(mob);
 
 // Call listener function at run time
 changeClass(mq_dsk);
@@ -47,3 +40,7 @@ setArrow(mq_mob);
 // mediaQuery.addListener(changePicture);
 mq_dsk.addListener(changeClass);
 mq_mob.addListener(setArrow);
+
+togg.addEventListener('click', () => {
+  setArrow();
+});
