4b9e79c2bd741729d39d0cb5cd9ef40b5712edc4
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-03-10 13:23:07 +0100

    define other routes & import components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
-import './styles/style.css';
+import './Styles/style.css';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Home } from './home/Home';
+import { About } from './About/About';
+import { Age } from './Age/Age';
+import { Contact } from './home/Home';
+import { Faq } from './Faq/Faq';
+import { Privacy } from './Privacy/Privacy';
+import { Prizes } from './Prizes/Prizes';
+import { Regulations } from './Regulations/Regulations';
 
 function App() {
   return (
     <main>
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/age" component={Age} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/faq" component={Faq} />
+        <Route path="/privacy" component={Privacy} />
+        <Route path="/prizes" component={Prizes} />
+        <Route path="/regulations" component={Regulations} />
       </Switch>
     </main>
   );
