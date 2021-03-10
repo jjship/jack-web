8471f9c788381f77d94f6e0d9d8715ea0a9b3ff6
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-03-10 13:12:21 +0100

    add Switch with Home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import './styles/style.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Home } from './home/Home';
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <main>
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </main>
+  );
 }
 
 export default App;
