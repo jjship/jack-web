cf00d386a2b29b41c46ac8e8b5bff47efbf0dae2
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-03-11 12:00:41 +0100

    uncomment contact import & route

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Home } from './Home/Home';
 import { About } from './About/About';
 // import { Age } from './Age/Age';
 import { Contact } from './Contact/Contact';
-// import { Faq } from './Faq/Faq';
+import { Faq } from './Faq/Faq';
 // import { Privacy } from './Privacy/Privacy';
 // import { Prizes } from './Prizes/Prizes';
 // import { Regulations } from './Regulations/Regulations';
@@ -17,8 +17,8 @@ function App() {
         <Route path="/about" component={About} />
         {/* <Route path="/age" component={Age} /> */}
         <Route path="/contact" component={Contact} />
-        {/* <Route path="/faq" component={Faq} />
-        <Route path="/privacy" component={Privacy} />
+        <Route path="/faq" component={Faq} />
+        {/* <Route path="/privacy" component={Privacy} />
         <Route path="/prizes" component={Prizes} />
         <Route path="/regulations" component={Regulations} /> */}
       </Switch>
