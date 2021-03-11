1823ce44d4217513e032dd0619e473d2afea60a5
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-03-11 11:11:45 +0100

    add About route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './Styles/style.css';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Home } from './Home/Home';
-// import { About } from './About/About';
+import { About } from './About/About';
 // import { Age } from './Age/Age';
 // import { Contact } from './home/Home';
 // import { Faq } from './Faq/Faq';
@@ -14,9 +14,9 @@ function App() {
     <main>
       <Switch>
         <Route path="/" component={Home} exact />
-        {/* <Route path="/about" component={About} />
-        <Route path="/age" component={Age} />
-        <Route path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+        {/* <Route path="/age" component={Age} /> */}
+        {/* <Route path="/contact" component={Contact} />
         <Route path="/faq" component={Faq} />
         <Route path="/privacy" component={Privacy} />
         <Route path="/prizes" component={Prizes} />
