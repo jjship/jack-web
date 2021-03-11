9df5ea3cf949bc5be2cdc2d8971390d880a80c37
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-03-11 15:09:59 +0100

    del useWIndowWidth import

--- src/Shared/Nav.js
+++ src/Shared/Nav.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useWindowWidth } from './useWindowWidth';
 
 export const Nav = () => {
   const [navClosed, setNavClosed] = useState(false);
