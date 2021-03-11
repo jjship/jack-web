aef643055ee0e04139d743c9288f0bd10bd0e39e
Jacek Skrzypek <jjskrzypek@gmail.com>
2021-03-11 11:30:41 +0100

    uncomment contact route & import

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Home } from './Home/Home';
 import { About } from './About/About';
 // import { Age } from './Age/Age';
-// import { Contact } from './home/Home';
+import { Contact } from './Contact/Contact';
 // import { Faq } from './Faq/Faq';
 // import { Privacy } from './Privacy/Privacy';
 // import { Prizes } from './Prizes/Prizes';
@@ -16,8 +16,8 @@ function App() {
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} />
         {/* <Route path="/age" component={Age} /> */}
-        {/* <Route path="/contact" component={Contact} />
-        <Route path="/faq" component={Faq} />
+        <Route path="/contact" component={Contact} />
+        {/* <Route path="/faq" component={Faq} />
         <Route path="/privacy" component={Privacy} />
         <Route path="/prizes" component={Prizes} />
         <Route path="/regulations" component={Regulations} /> */}
