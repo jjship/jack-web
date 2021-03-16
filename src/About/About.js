7c6cec2afedd1c89b54fbe8857efef03771dbe42
JACEK SKRZYPEK <devontheroof@gmail.com>
2021-03-16 13:15:11 +0100

    class to className

--- src/About/About.js
+++ src/About/About.js
@@ -14,30 +14,30 @@ export const About = () => {
 
   return (
     <div
-      class="main-dsk l-wrapper l-a-wrapper js-wrapper"
+      className="main-dsk l-wrapper l-a-wrapper js-wrapper"
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
       <NavDsk />
-      <div class="l-container l-a-container u-uppercase">
-        <a href="/" class="c-close l-close">
+      <div className="l-container l-a-container u-uppercase">
+        <a href="/" className="c-close l-close">
           &#10005;
         </a>
-        <div class="l-title c-title -f-jasper-caps">
+        <div className="l-title c-title -f-jasper-caps">
           <p>
             O Jack’u Danielu
             <br /> i Jack’u Daniel’sie
           </p>
         </div>
-        <div class="l-a-flex">
-          <div class="l-a-flex-item l-a-grid-video">
-            <img src={zaslepka} alt="film" class="l-a-zaslepka" />
+        <div className="l-a-flex">
+          <div className="l-a-flex-item l-a-grid-video">
+            <img src={zaslepka} alt="film" className="l-a-zaslepka" />
           </div>
-          <div class="l-a-flex-item l-a-flex__text -f-myriad-cond u-golden u-no-text-transform">
-            <p class="l-a-p-jak c-a-p-jak">
+          <div className="l-a-flex-item l-a-flex__text -f-myriad-cond u-golden u-no-text-transform">
+            <p className="l-a-p-jak c-a-p-jak">
               Jak się bardzo chce, to się musi udać! Jack Daniel chciał
               wyprodukować najlepszą whiskey na świecie. I udało się! Jak?
             </p>
-            <p class="l-a-p-moze c-a-p-moze">
+            <p className="l-a-p-moze c-a-p-moze">
               Może to woda, której źródło bije w jaskini, może to dębowe beczki
               używane tylko raz, może to szczęśliwa liczba „7”, która zdobi
               etykietę od 150 lat tak samo kwadratowej butelki. To na pewno
@@ -46,18 +46,18 @@ export const About = () => {
               wiedział Jack Daniel:
             </p>
           </div>
-          <div class="c-a-slogan-today l-a-slogan-container l-a-flex__text">
-            <p class="c-a-slogan-dzis l-a-slogan-dzis -f-motlow">dziś</p>
-            <p class="c-a-slogan-zdarza l-a-slogan-zdarza -f-franklin-demi">
+          <div className="c-a-slogan-today l-a-slogan-container l-a-flex__text">
+            <p className="c-a-slogan-dzis l-a-slogan-dzis -f-motlow">dziś</p>
+            <p className="c-a-slogan-zdarza l-a-slogan-zdarza -f-franklin-demi">
               zdarza się
             </p>
-            <p class="c-a-slogan-tylko l-a-slogan-tylko -f-jasper-caps">
+            <p className="c-a-slogan-tylko l-a-slogan-tylko -f-jasper-caps">
               tylko raz
             </p>
           </div>
         </div>
         <Carousel />
-        <a href="/" class="c-btn__zamknij l-p-btn__zamknij -f-myriad-cond">
+        <a href="/" className="c-btn__zamknij l-p-btn__zamknij -f-myriad-cond">
           zamknij
         </a>
       </div>
