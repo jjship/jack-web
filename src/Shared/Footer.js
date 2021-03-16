f9f85249b341d6ab02f6f30b6561b93554f89852
JACEK SKRZYPEK <devontheroof@gmail.com>
2021-03-16 11:34:15 +0100

    change font weight footer

--- src/Shared/Footer.js
+++ src/Shared/Footer.js
@@ -14,46 +14,51 @@ export const Footer = () => {
           <a href="http://www.pijodpowiedzialnie.pl" className="c-footer-link">
             www.pijodpowiedzialnie.pl
           </a>
-          <br />
-          <a
-            className="c-footer-link"
-            href="https://legal.brown-forman.com/terms-of-use/polish"
-          >
-            Warunki korzystania |{' '}
-          </a>
-          <a
-            className="c-footer-link"
-            href="https://legal.brown-forman.com/privacy-policy/polish"
-          >
-            Polityka prywatności |{' '}
-          </a>
-          <a
-            className="c-footer-link"
-            href="https://legal.brown-forman.com/cookie-policy/polish"
-          >
-            Polityka cookies
-          </a>
-          <br />
-          Jack Daniel's i Old No.7 są zarejestrowanymi znakami towarowymi. ©2021
-          Jack Daniel's.
-          <br />
-          Wszystkie prawa zastrzeżone.
-          <br />
-          Aby dowiedzieć się więcej o odpowiedzialnym spożywaniu alkoholu,
-          odwiedź{' '}
-          <a href="http://www.pijodpowiedzialnie.pl" className="c-footer-link">
-            www.pijodpowiedzialnie.pl
-          </a>{' '}
-          and{' '}
-          <a
-            href="http://www.OurThinkingAboutDrinking.com"
-            className="c-footer-link"
-          >
-            OurThinkingAboutDrinking.com
-          </a>{' '}
-          Wszystkie inne znaki i nazwy należą do odpowiednich podmiotów.
-          Materiał przeznaczony dla osób pełnoletnich. Nie przekazuj go osobom w
-          wieku poniżej 18 lat.
+          <span style={{fontWeight: "400"}}>
+            <br />
+            <a
+              className="c-footer-link"
+              href="https://legal.brown-forman.com/terms-of-use/polish"
+            >
+              Warunki korzystania |{' '}
+            </a>
+            <a
+              className="c-footer-link"
+              href="https://legal.brown-forman.com/privacy-policy/polish"
+            >
+              Polityka prywatności |{' '}
+            </a>
+            <a
+              className="c-footer-link"
+              href="https://legal.brown-forman.com/cookie-policy/polish"
+            >
+              Polityka cookies
+            </a>
+            <br />
+            Jack Daniel's i Old No.7 są zarejestrowanymi znakami towarowymi.
+            ©2021 Jack Daniel's.
+            <br />
+            Wszystkie prawa zastrzeżone.
+            <br />
+            Aby dowiedzieć się więcej o odpowiedzialnym spożywaniu alkoholu,
+            odwiedź{' '}
+            <a
+              href="http://www.pijodpowiedzialnie.pl"
+              className="c-footer-link"
+            >
+              www.pijodpowiedzialnie.pl
+            </a>{' '}
+            and{' '}
+            <a
+              href="http://www.OurThinkingAboutDrinking.com"
+              className="c-footer-link"
+            >
+              OurThinkingAboutDrinking.com
+            </a>{' '}
+            Wszystkie inne znaki i nazwy należą do odpowiednich podmiotów.
+            Materiał przeznaczony dla osób pełnoletnich. Nie przekazuj go osobom
+            w wieku poniżej 18 lat.
+          </span>
         </p>
       </div>
     </footer>
